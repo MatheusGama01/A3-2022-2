5a4c12ff988c37157cdc016f320e182d08560773
Matheus <92063486+MatheusGama01@users.noreply.github.com>
2022-12-01 23:46:22 -0300

    Alterada Strings para conexão

--- A3_2022_2/src/main/java/DAO/ConexaoDAO.java
+++ A3_2022_2/src/main/java/DAO/ConexaoDAO.java
@@ -8,9 +8,9 @@ import java.sql.SQLException;
 public class ConexaoDAO {
 
     Connection conn = null;
-    String url = "jdbc:mysql://localhost:3306/teste_a3";
-    String usuario = "root";
-    String senha = "@ServidorMySQL@";
+    String url = "jdbc:mysql://localhost:3306/nomebanco";
+    String usuario = "";
+    String senha = "";
 
     //Inicializa a conexão com o banco de dados.
     public Connection conectaBD() throws NaoFoiPossivelEstabelecerConexaoComOBancoDeDadosException {
